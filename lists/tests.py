9c63e8bd2c35d25b510d469be40bf8bdb34c84b2
otaree <deep.yen.ur.hash@gmail.com>
2017-06-29 19:29:49 +0530

    Front page HTML now generated from a template

--- lists/tests.py
+++ lists/tests.py
@@ -1,8 +1,4 @@
 from django.test import TestCase
-from django.core.urlresolvers import resolve
-from django.http import HttpRequest
-
-from lists.views import home_page
 
 # Create your tests here.
 
